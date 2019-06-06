157321a2ea13d34f3df98776d27165381fc1f50e
luciaemiliaeu <luciaemilia72@gmail.com>
2019-06-06 09:51:01 -0300

    Atualização de imports

--- script.py
+++ script.py
@@ -1,8 +1,5 @@
 import labelClassify
 import pandas as pd
-import numpy as np 
-import matplotlib.pyplot as plt 
-from sklearn.decomposition import PCA
 import warnings
 
 warnings.filterwarnings('once')
