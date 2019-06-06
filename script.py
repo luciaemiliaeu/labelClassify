98b047d9f4d5dd49af52f8e585e120001acc515d
luciaemiliaeu <luciaemilia72@gmail.com>
2019-06-06 09:49:13 -0300

    Separação de arquivos

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import warnings
 
 warnings.filterwarnings('once')
 
-chamadas = [("sementes.csv", "EFD", [3,3,3,3,3,3,3],5),("vidros.csv", "EWD", [4,4,4,4,4,4,4,4,4],15)]
+chamadas = [("./databases/iris.csv", "EFD", [3,3,3,3], 10),("./databases/sementes.csv", "EFD", [3,3,3,3,3,3,3],5),("./databases/vidros.csv", "EWD", [4,4,4,4,4,4,4,4,4],15)]
 
 
 for i in chamadas:
@@ -20,14 +20,8 @@ for i in chamadas:
 			rotular = labelClassify.LABELROTULATOR(bd, per_tst, i[1], i[2], 60, i[3], 10).metrics
 			rotular.insert(0, per_tst)
 			results.loc[results.index.size,:] = rotular
-			results.to_csv('Resultado-'+str(i[0].split('.')[0])+'.csv', sep=',' )
+			results.to_csv('./results/Resultado-'+str(i[0].split('.')[0])+'.csv', sep=',' )
 		per_tst += 0.10
 		per_tst = round(per_tst, 2)
 		
 
-
-'''
-
-("iris.csv", "EFD", [3,3,3,3], 10),("vidros.csv", "EWD", [4,4,4,4,4,4,4,4,4],15),
-
-'''
